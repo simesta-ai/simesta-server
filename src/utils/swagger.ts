65843bd2cd75b054c15fb34d894b522a15ccbe4d
Kingsley Chukwudi Ihemelandu <kijuchihe@gmail.com>
2024-08-16 16:17:51 +0100

    Basic config

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,4 +1,15 @@
-import {} from "express"
-import swaggerJsdoc from "swagger-jsdoc"
-import swaggerUi from "swagger-ui-express"
+import {} from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'simesta ai api docs',
+      version: '1.0.0',
+      description: 'API for simesta AI platform',
+    },
+    components: {},
+  },
+};
