1d53a7da6eccd4d8001ee94e6661fec3476a59aa
Kingsley Ihemelandu <kijuchihe@gmail.com>
2024-09-13 13:25:39 +0100

    Ref: Refactor the code a bit

--- src/api/v1/modules/course/routes.ts
+++ src/api/v1/modules/course/routes.ts
@@ -1,11 +1,9 @@
 import express from 'express'
-import UserController from '../user/controllers'
 import upload from '../../../../libs/utils/services/multer'
 import checkTotalFileSize from '../../../../libs/middlewares/validators/file.validator'
 import CourseController from './controllers'
 const router = express.Router()
 
-const userController = new UserController()
 const courseController = new CourseController()
 
 router.route('/:userId/course').get(courseController.getAllCourses).post(
--- src/api/v1/modules/course/services/createCourse.ts
+++ src/api/v1/modules/course/services/createCourse.ts
@@ -23,6 +23,53 @@ const userRepository = new UserRepository()
 const courseRepository = new CourseRepository()
 const uploadFilesToBucket = new UploadFilesToBucket()
 
+// Can't we use this fucntion?
+
+const AIGenContent = async (title: string) => {
+  const validCourseTitle = await AIGen.confirmCourseTitle(title)
+  if (!validCourseTitle) return
+  const courseDescription = await AIGen.generateCourseDescription(title)
+  const courseCategory = await AIGen.generateCoursecategory(title)
+  const courseImage = await AIGen.generateCourseImage(title)
+
+  return { courseDescription, courseCategory, courseImage }
+}
+
+const parseFiles = async (files: File[]) => {
+  let error: CustomError | null = null
+  console.log('Starting work on files')
+  // Add files to course and generate course topics
+  const filePaths = files.map((file: any) => file.path)
+  const fileData = await Promise.all(
+    filePaths.map(async (filePath: string) => {
+      const { fileUrl, UploadError } = await uploadFilesToBucket.uploadFiles(
+        filePath
+      )
+      if (UploadError) {
+        error = UploadError
+      }
+      const { data, fileError } = await fileService.parseFile(filePath)
+      if (fileError) {
+        error = fileError
+      }
+      if (fileUrl && data) {
+        await redisService.client.set(fileUrl, JSON.stringify(data))
+      }
+
+      return { data, fileUrl }
+    })
+  )
+
+  filePaths.forEach((filePath: string) => {
+    fs.unlinkSync(filePath)
+  })
+
+  const fileUrls = fileData.map((file: any) => file.fileUrl)
+  const fileContent = fileData.map((file: any) => file.data)
+
+  return { fileUrls, fileContent, error }
+}
+
 const createCourse = async ({
   userId,
   title,
@@ -41,6 +88,7 @@ const createCourse = async ({
       error: new AuthError('User does not exist, cannot create course'),
     }
   }
+  // const {courseDescription, courseCategory, courseImage} = AIGenContent(title)
   const validCourseTitle = await AIGen.confirmCourseTitle(title)
   if (!validCourseTitle)
     return {
@@ -51,15 +99,16 @@ const createCourse = async ({
   const courseCategory = await AIGen.generateCoursecategory(title)
   const courseImage = await AIGen.generateCourseImage(title)
 
-  console.log('Done with the course resource generation')
+  // console.log('Done with the course resource generation')
 
   let fileUrls: string[] = []
   let fileContent: string[] = []
 
   if (files) {
+    // const {fileUrls, fileContent, error} = parseFiles(files)
     console.log('Starting work on files')
     // Add files to course and generate course topics
-    const filePaths = files.map((file: any) => file.path)
+    const filePaths: string[] = files.map((file: any) => file.path)
     const fileData = await Promise.all(
       filePaths.map(async (filePath: string) => {
         const { fileUrl, UploadError } = await uploadFilesToBucket.uploadFiles(
@@ -74,7 +123,6 @@ const createCourse = async ({
         }
         if (fileUrl && data) {
           await redisService.client.set(fileUrl, JSON.stringify(data))
-
         }
 
         return { data, fileUrl }
