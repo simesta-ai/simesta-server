3d99b6c4a8dd5ef785fed611159a635b29206d73
Danny944 <fadehandaniel2006@gmail.com>
2024-11-07 18:46:22 +0100

    removed unused file

--- src/api/v1/modules/notifications/services/getAllExpoTokens.ts
+++
@@ -1,9 +0,0 @@
-import { redisService } from "../../../../../libs/utils/services/redis";
-
-const getAllTokenKeys = async () => {
-    let cursor = '0'
-    let tokens = []
-    do{
-        const [newCursor, keys] = await redisService.scan(cursor, 'MATCH', 'expoToken:*')
-    }while(cursor != '0')
-}
