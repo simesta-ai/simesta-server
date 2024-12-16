37641b52bf3ac9f6a6ff721d87ad8c62e9a1f89f
Samuel Emeka <samuel.emeka@kibo.school>
2024-12-16 18:47:49 +0100

    feat: Update import paths for recommendations engine and controller

--- src/api/v1/modules/recommendations/recommendations.controller.ts
+++ src/api/v1/modules/recommendations/recommendations.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express"
 import { RecommendationEngine } from "./recommendations.engine"
-import { generateJsonSuccessResponse } from "@/shared/utils/generateJsonResponse"
-import { catchHandlerAsync } from "@/shared/utils/catch-handler-async"
+import { generateJsonSuccessResponse } from "../../../../shared/utils/generateJsonResponse"
+import { catchHandlerAsync } from "../../../../shared/utils/catch-handler-async"
 
 export class RecommendationsController {
   private recommendationEngine = new RecommendationEngine()
   public getRecommendations = catchHandlerAsync(async (req: Request, res: Response) => {
-    const userId = req.user?.id!
+    const { userId } = req.body
     const recommendations = await this.recommendationEngine.generateRecommendations(userId)
     res.json(generateJsonSuccessResponse('Recommendations fetched successfully', recommendations))
   })
